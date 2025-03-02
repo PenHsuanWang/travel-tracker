b4998cc48c246537c91a68ae11c10c9a34356ee0
PenHsuanWang <willy118116@gmail.com>
2025-03-02 17:16:19 +0800

    provide function to re-generate the map after selected gpx file

--- server/src/models/map_request.py
+++ server/src/models/map_request.py
@@ -1,5 +1,7 @@
+# server/src/models/map_request.py
 from pydantic import BaseModel
-
+from typing import Optional
 
 class MapRequest(BaseModel):
     layer: str
+    center: Optional[tuple[float, float]] = None  # (latitude, longitude)
